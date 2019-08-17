9da2fdbbc46b57b7aafb2bc02a8bb6ac0ed275dd
Chris Sidi <chrisid@microsoft.com>
2019-08-17 01:03:06 -0400

    Test warning and error

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ async function run() {
   try {
     const myInput = core.getInput('myInput');
     core.debug(`Hello ${myInput}`);
+    core.warning(`I'm warning you!`);
+    core.error(`To err is human`);
   } catch (error) {
     core.setFailed(error.message);
   }
